0e17c2d925a67aab84d24f027a8cdbe9ec757576
Adam debbagh <adamdebbagh@gmail.com>
2015-08-21 08:54:38 -0400

    checked boxes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,27 @@
+// fix sidebar navigation transition problem
 $(document).ready(function(){
     $('body').removeClass('notransition');
 });
+
+// Toggle group button checkbox Glyphicons
+$(document).ready(function(){
+  $("[type='checkbox']").on('change', function(){
+      if($(this).prop('checked')) {
+          $(this).prev()
+                 .removeClass('glyphicon-unchecked')
+                 .addClass('glyphicon-ok-circle');
+      }else {
+          $(this).prev()
+              .removeClass('glyphicon-ok-circle')
+              .addClass('glyphicon-unchecked');
+      }
+  });
+
+    $('input:checked').prev()
+        .removeClass('glyphicon-unchecked')
+        .addClass('glyphicon-ok-circle');
+    $('input:checked').parent()
+        .addClass('active');
+});
+
+
